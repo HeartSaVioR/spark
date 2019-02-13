167870b03c8b3a59de607151a69dcdbfb434ec1d
Jungtaek Lim (HeartSaVioR) <kabhwan@gmail.com>
2019-02-13 19:25:38 +0900

    Address review comment from mikedias

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/FileStreamSource.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/FileStreamSource.scala
@@ -272,7 +272,7 @@ class FileStreamSource(
           validFileEntities.foreach(sourceCleaner.archive)
 
         case CleanSourceMode.DELETE =>
-          validFileEntities.foreach(sourceCleaner.remove)
+          validFileEntities.foreach(sourceCleaner.delete)
 
         case _ =>
       }
@@ -376,7 +376,7 @@ object FileStreamSource {
       }
     }
 
-    def remove(entry: FileEntry): Unit = {
+    def delete(entry: FileEntry): Unit = {
       val curPath = new Path(new URI(entry.path))
       try {
         logDebug(s"Removing completed file $curPath")
