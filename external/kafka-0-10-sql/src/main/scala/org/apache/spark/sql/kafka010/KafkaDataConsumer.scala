0e1374eed84d0961e7a99c1b295e69fdd0a938a2
Jungtaek Lim (HeartSaVioR) <kabhwan@gmail.com>
2019-03-13 22:46:17 +0900

    Add more log message for creating new Kafka consumer

--- external/kafka-0-10-sql/src/main/scala/org/apache/spark/sql/kafka010/KafkaDataConsumer.scala
+++ external/kafka-0-10-sql/src/main/scala/org/apache/spark/sql/kafka010/KafkaDataConsumer.scala
@@ -105,6 +105,8 @@ private[kafka010] class InternalKafkaConsumer(
 
   /** Create a KafkaConsumer to fetch records for `topicPartition` */
   private def createConsumer: KafkaConsumer[Array[Byte], Array[Byte]] = {
+    logWarning(s"DEBUG: creating new Kafka consumer for $topicPartition")
+
     val updatedKafkaParams = KafkaConfigUpdater("executor", kafkaParams.asScala.toMap)
       .setAuthenticationConfigIfNeeded()
       .build()
