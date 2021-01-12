4511bb3ac8afd473850ce29c2d02a2797316974f
Jungtaek Lim <kabhwan.opensource@gmail.com>
2021-01-12 09:37:02 +0900

    WIP adding write capability on state table, still work in progress...

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/state/StateTable.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/state/StateTable.scala
@@ -21,8 +21,12 @@ import java.util
 import scala.collection.JavaConverters._
 
 import org.apache.spark.sql.{AnalysisException, SparkSession}
-import org.apache.spark.sql.connector.catalog.{SupportsRead, Table, TableCapability}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.connector.catalog.{SupportsRead, SupportsWrite, Table, TableCapability}
+import org.apache.spark.sql.connector.distributions.{Distribution, Distributions}
+import org.apache.spark.sql.connector.expressions.{Expression, FieldReference, SortOrder}
 import org.apache.spark.sql.connector.read.ScanBuilder
+import org.apache.spark.sql.connector.write.{BatchWrite, DataWriter, DataWriterFactory, LogicalWriteInfo, PhysicalWriteInfo, RequiresDistributionAndOrdering, Write, WriteBuilder, WriterCommitMessage}
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 
@@ -33,7 +37,7 @@ class StateTable(
     version: Long,
     operatorId: Int,
     storeName: String)
-  extends Table with SupportsRead {
+  extends Table with SupportsRead with SupportsWrite {
 
   import StateTable._
 
@@ -67,8 +71,57 @@ class StateTable(
       true
     }
   }
+
+  override def newWriteBuilder(info: LogicalWriteInfo): WriteBuilder = {
+    val options = info.options()
+    // TODO: how we could support the case where end users want to retain the number of
+    //  partitions during rewrite?
+    require(options.containsKey("numPartitions"), "Option 'numPartitions' is missing.")
+    val numPartitions = options.get("numPartitions").toInt
+
+    new WriteBuilder {
+      override def build(): Write = new Write with RequiresDistributionAndOrdering {
+        override def requiredDistribution(): Distribution = {
+          val keySchema = SchemaUtil.getSchemaAsDataType(schema, "key").asInstanceOf[StructType]
+          // TODO: expand this to cover multi-depth (nested) columns (do we want to cover it?)
+          val fullPathsForKeyColumns = keySchema.map { key =>
+            FieldReference(s"key.${key.name}")
+          }.toArray[Expression]
+          Distributions.clustered(fullPathsForKeyColumns, numPartitions)
+        }
+
+        override def requiredOrdering(): Array[SortOrder] = Array.empty[SortOrder]
+
+        override def toBatch: BatchWrite = {
+          new StateBatchWrite
+        }
+      }
+    }
+  }
+
+  class StateBatchWrite extends BatchWrite {
+    override def createBatchWriterFactory(info: PhysicalWriteInfo): DataWriterFactory = {
+      new StateBatchDataWriterFactory
+    }
+
+    override def commit(messages: Array[WriterCommitMessage]): Unit = {
+      // FIXME: anything specific to do?
+      //  1. updates schema file?
+      //  2. updates the number of partitions?
+    }
+
+    override def abort(messages: Array[WriterCommitMessage]): Unit = {
+      // FIXME: should we clean up the partial writes here?
+    }
+  }
+
+  class StateBatchDataWriterFactory extends DataWriterFactory {
+    override def createWriter(partitionId: Int, taskId: Long): DataWriter[InternalRow] = {
+      // FIXME: ...fill here...
+    }
+  }
 }
 
 object StateTable {
-  private val CAPABILITY = Set(TableCapability.BATCH_READ).asJava
+  private val CAPABILITY = Set(TableCapability.BATCH_READ, TableCapability.BATCH_WRITE).asJava
 }
